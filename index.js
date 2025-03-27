6b7f4c29d197b3ffdfb40ce3ce99b5544932e58e
Abhishek Zatakiya <114485563+Zatakiya2510@users.noreply.github.com>
2025-03-27 18:10:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const LOCAL_URL = 'http://localhost:3000';
 
 // ✅ Optimized CORS Setup
 const corsOptions = {
-    origin: "*",
+    origin: [FRONTEND_URL,LOCAL_URL],
     credentials: true,
     methods: "GET, POST, PUT, DELETE, OPTIONS",
     allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization"
